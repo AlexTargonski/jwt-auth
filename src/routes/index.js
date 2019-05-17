9bd185ac1fec13bdcef3a4a93d6cd47bc2280f63
Oleksandr <targonsky97@gmail.com>
2019-05-17 09:08:15 +0300

    add redirection if token exists

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Route,
   Switch,
+  Redirect
 }                              from 'react-router-dom';
 
 import LoginPage               from '../components/LoginPage';
@@ -10,9 +11,13 @@ import SignUpPage              from '../components/SignUpPage';
 import HomePage                from '../components/HomePage';
 import PrivateRoute            from './PrivateRoute';
 
+const token = localStorage.getItem('token');
+
 export default (
   <Router>
     <Switch>
+      {token  && <Redirect from="/" to="/home" exact />}
+      {token  && <Redirect from="/sign_up" to="/home" exact />}
       <Route        exact path="/"        component={LoginPage} />
       <Route        exact path="/sign_up" component={SignUpPage} />
       <PrivateRoute exact path="/home"    component={HomePage} />
